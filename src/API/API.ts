5522a096026234e173a3692c7053acbafa9586fc
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-01-12 21:41:49 -0500

    removed extra slash in api url concat

--- src/API/API.ts
+++ src/API/API.ts
@@ -3,21 +3,21 @@ import {BASE_URL, CORS_API_URL} from "../constants/APIConstants";
 
 export default {
   getItems: function() {
-    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "/items" : BASE_URL + "/items";
+    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "items" : BASE_URL + "items";
     return axios({
       method: "get",
       url: url
     });
   },
   getOrders: function() {
-    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "/orders" : BASE_URL + "/orders";
+    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "orders" : BASE_URL + "orders";
     return axios({
       method: "get",
       url: url
     });
   },
   getRecipes: function() {
-    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "/recipes" : BASE_URL + "/recipes";
+    let url = process.env.NODE_ENV === "development" ? CORS_API_URL + BASE_URL + "recipes" : BASE_URL + "recipes";
     return axios({
       method: "get",
       url: url
